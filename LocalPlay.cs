c2f5a887bef772dd0ad1e14aa0a91d8485dcc3ca
Jack Kimmins <50149309+jackkimmins@users.noreply.github.com>
2023-01-17 14:54:21 +0000

    General Uploads

--- LocalPlay.cs
+++ LocalPlay.cs
@@ -29,54 +29,30 @@ public class LocalPlay
 
             moveNum++;
             cText.WriteLine("Player " + board.currentPlayerTurn + " make a move: ", "CONNECT4", ConsoleColor.White);
-            
-            if (board.currentPlayerTurn == 1)
+
+            int move = -1;
+
+            //Ensure that the move is valid an int between 1 and 7
+            while (move < 1 || move > 7 || board.board[0, move - 1] != 0)
             {
-                int move = -1;
+                Console.Write("Enter column number (1-7): ");
+                string input = Console.ReadLine() ?? "";
 
-                //Ensure that the move is valid an int between 1 and 7
-                while (move < 1 || move > 7 || board.board[0, move - 1] != 0)
+                if (input == "?")
                 {
-                    Console.Write("Enter column number (1-7): ");
-                    string input = Console.ReadLine() ?? "";
-
-                    if (input == "?")
-                    {
-                        move = miniMax.GetBestMove(board).Column;
-                        break;
-                    }
-                    else if (input == "q")
-                    {
-                        Console.WriteLine("Quitting...");
-                        return;
-                    }
-
-                    if (!int.TryParse(input, out move)) move = -1;
+                    move = miniMax.GetBestMove(board).Column;
+                    break;
+                }
+                else if (input == "q")
+                {
+                    Console.WriteLine("Quitting...");
+                    return;
                 }
 
-                board.MakeMove(move);
-
-                // Random rand = new Random();
-                // playersMove = rand.Next(1, 7);
-                // // playersMove = miniMax.GetBestMove(board).Column;
-
-                // //If the opponent can win on the next move, pick a different move
-                // if (board.WinningMove(board) != 0)
-                // {
-                //     while (board.WinningMove(board) != 0)
-                //     {
-                //         playersMove = rand.Next(1, 7);
-                //     }
-                // }
-
-                // board.MakeMove(playersMove);
+                if (!int.TryParse(input, out move)) move = -1;
             }
-            else
-            {
-                ReturnMove bestMove = miniMax.GetBestMove(board);
-                board.MakeMove(bestMove.Column);
-                cText.WriteLine("Best move: " + bestMove.Column + " with score: " + bestMove.Score + " and iterations: " + bestMove.Iterations);
-            }
+
+            board.MakeMove(move);
         }
 
         Play();
